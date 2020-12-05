1b0e88bb181e1852f4e33aae9d98a63e428e1dec
sergi <sergi@192.168.1.138>
2020-12-05 18:09:28 +0100

    Semana4

---
+++ Ejercicio4/src/Ejercicio4.java
@@ -0,0 +1,19 @@
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		int minelefante = 10;
+		if (minelefante >= 1) {
+			System.out.println(
+					"1 Elefante se balanceaba sobre la tela de una araña, como veia que no se caia, fue a llamar a otro elefante");
+			for (int i = 2; i <= minelefante; i++) {
+				System.out.println(i + "1 Elefante se balanceaba sobre la tela de una araña,"
+						+ "como veia que no se caia, fue a llamar a otro elefante");
+
+			}
+			System.out.println("la tela se ha roto por el peso");
+		}
+
+	}
+
+}
